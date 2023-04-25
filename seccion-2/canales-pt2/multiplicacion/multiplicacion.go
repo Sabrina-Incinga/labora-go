7db96eabbfe47c6e356adfaa617765be453a294f
Sabrina-Incinga <sabrinagincinga@gmail.com>
2023-04-25 00:25:29 -0300

    corrección producto de matrices, orden de las filas arreglado

--- seccion-2/canales-pt2/multiplicacion/multiplicacion.go
+++ seccion-2/canales-pt2/multiplicacion/multiplicacion.go
@@ -10,82 +10,89 @@ Dadas dos matrices A y B, crea un programa en Go que realice la multiplicación 
 //len == nro filas 
 //len de elementos == nro de columnas 
 //len x lenElementos
+type FilaResultado struct {
+    Indice int
+    Fila   []int
+}
 
-func MultiplicarMatrices(matrizA [][]int, matrizB [][]int){
-	//validamos que sean matrices rectangulares, es decir, matrices válidas para multiplicar
-	var validA, validB bool
-	var nroColumnasA int = 0
-	matrizResultado := make([][]int, len(matrizA))
-	if len(matrizA) != 0 {
-		nroColumnasA = len(matrizA[0])
-		for _, v := range matrizA {
-			if nroColumnasA == len(v) {
-				validA = true
-			}else {
-				validA = false
-			}
-		}
-	}
-	
-	nroFilasB := len(matrizB)
-	if nroFilasB != 0 {
-		nroColumnasB := len(matrizB[0])
-		for _, v := range matrizB {
-			if nroColumnasB == len(v) {
-				validB = true
-			}else {
-				validB = false
-			}
-		}
-	}
-	//validamos que las matrices válidas puedan multiplicarse, verificando 	que el nro de columnas de matrizA sea igual al nro de filas de la matriB
-	if validA && validB {
-		if nroColumnasA == nroFilasB {
-			var ch chan []int = make(chan []int)
-			for i := 0; i < len(matrizA); i++ {
-				go multiplyRow(matrizA, i, matrizB, ch)
-			}
-
-			for i := 0; i < len(matrizA); i++ {
-				matrizResultado[i] = <-ch
+// valida si una matriz es rectangular, es decir, que todas sus filas tienen el mismo número de columnas, devuelve un booleano y el número de columnas en caso de ser matriz rectangular
+func esMatrizRectangular(matriz [][]int) (int, bool) {
+	var nroColumnas int
+	var valid bool
+	if len(matriz) != 0 {
+		nroColumnas = len(matriz[0])
+		for _, v := range matriz {
+			if nroColumnas == len(v) {
+				valid = true
+			} else {
+				valid = false
 			}
 		}
 	}
 
-	printMatrix(matrizResultado)
-	//PROBLEMA: no es posible predecir qué go rutina se ejecutará primero, por lo que la matriz resultante no necesariamente se forma en el orden correcto
+	if !valid {
+		return 0, valid
+	}
+	return nroColumnas, valid
 }
 
 // multiplica una fila de la matriz A por la matriz B y envía el resultado por el canal result
-func multiplyRow(A [][]int, row int, B [][]int, result chan<- []int) {
-	rowA := A[row]
-	rowResult := make([]int, len(B[0]))
+func multiplicarFila(A [][]int, fila int, B [][]int, resultado chan<- FilaResultado) {
+	filaA := A[fila]
+	filaResultado := make([]int, len(B[0]))
 	for i := 0; i < len(B[0]); i++ {
 		colB := make([]int, len(B))
 		for j := 0; j < len(B); j++ {
 			colB[j] = B[j][i]
 		}
-		rowResult[i] = dotProduct(rowA, colB)
+		filaResultado[i] = productoPunto(filaA, colB)
 	}
-	result <- rowResult
+	resultado <- FilaResultado{Indice: fila, Fila: filaResultado}
 }
 
 // realiza el producto punto entre dos vectores
-func dotProduct(v1 []int, v2 []int) int {
-	result := 0
+func productoPunto(v1 []int, v2 []int) int {
+	resultado := 0
 	for i := 0; i < len(v1); i++ {
-		result += v1[i] * v2[i]
+		resultado += v1[i] * v2[i]
 	}
-	return result
+	return resultado
 }
 
 // imprime una matriz en la consola
-func printMatrix(matrix [][]int) {
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[0]); j++ {
-			fmt.Printf("matrix[%d][%d]: %d ",i, j, matrix[i][j])
+func imprimirMatriz(matriz [][]int) {
+	for i := 0; i < len(matriz); i++ {
+		for j := 0; j < len(matriz[0]); j++ {
+			fmt.Printf("[%d][%d]: %d ",i, j, matriz[i][j])
 		}
 		fmt.Println()
 	}
 	fmt.Println()
+}
+
+func MultiplicarMatrices(matrizA [][]int, matrizB [][]int){
+	//validamos que sean matrices rectangulares, es decir, matrices válidas para multiplicar
+	nroColumnasA, esRectangularA := esMatrizRectangular(matrizA)
+	_, esRectangularB := esMatrizRectangular(matrizB)
+	nroFilasB := len(matrizB)
+	matrizResultado := make([][]int, len(matrizA))
+	
+	//validamos que las matrices válidas puedan multiplicarse, verificando 	que el nro de columnas de matrizA sea igual al nro de filas de la matriB
+	if esRectangularA && esRectangularB {
+		if nroColumnasA == nroFilasB {
+			ch := make(chan FilaResultado, len(matrizA))
+			for i := 0; i < len(matrizA); i++ {
+				go multiplicarFila(matrizA, i, matrizB, ch)
+			}
+
+			for i := 0; i < len(matrizA); i++ {
+				rowResult := <-ch
+				matrizResultado[rowResult.Indice] = rowResult.Fila
+			}
+		}
+	}
+
+	imprimirMatriz(matrizResultado)
+	//PROBLEMA: no es posible predecir qué go rutina se ejecutará primero, por lo que la matriz resultante no necesariamente se forma en el orden correcto
+	//SOLUCIÓN: dado que el número de las filas de la matriz solución coincide con el de la matriz a, se envía por el canal de resultado tanto el número de fila, que enviamos por parámetro en la función multiplyRow, como la fila resultante 
 }
